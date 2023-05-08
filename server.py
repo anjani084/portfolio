30335df0ea088e429a028dc0f79eca102506628e
Anjani Mishra <91820285+anjani084@users.noreply.github.com>
2023-05-09 02:55:23 +0530

    Update2.0 server.py

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, url_for, request
 import csv
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(port=8080)
+    
 @app.route('/')
 def my_home():
     return render_template('index.html')
