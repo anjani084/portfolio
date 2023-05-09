7bef938637ce9fe9d32bb90ca328f71ed1a190c6
Anjani Mishra <91820285+anjani084@users.noreply.github.com>
2023-05-09 18:08:25 +0530

    Update server.py file 6.0

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request
 import csv
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__,template_folder='templates', static_folder='static')
 
 @app.route('/')
 def my_home():
